3943217c2f8e19f4b67ee4c3b1911fbad9faa569
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-05-04 14:06:37 +0630

    banner text

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from 'react';
 import marqueeIcon from '../assets/img/marquee.png';
 import useFetch from '../hooks/useFetch';
 const Marquee = () => {
-    const { data: bannerText, error, loading } = useFetch('https://livesportapi.online/api/bannerText');
-    // console.log(bannerText);
-    return (<div className="marqueeContainer  mx-2">
-        <img src={marqueeIcon} className='soundIcon' />
+  const {
+    data: bannerText,
+    error,
+    loading,
+  } = useFetch('https://theonemmslot.online/api/bannerText');
+  // console.log(bannerText);
+  return (
+    <div className='marqueeContainer  mx-2'>
+      <img src={marqueeIcon} className='soundIcon' />
 
-        <marquee className=' fw-bold  ' behavior="" direction="left">
-            <span>{bannerText?.text}</span>
-        </marquee>
+      <marquee className=' fw-bold  ' behavior='' direction='left'>
+        <span>{bannerText?.text}</span>
+      </marquee>
     </div>
+  );
+};
 
-    )
-}
-
-export default Marquee
+export default Marquee;
